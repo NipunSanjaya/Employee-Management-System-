f2f677fc463031f53773934f8efc36f984f41e14
NipunSanjaya <you@example.com>
2024-05-18 17:00:27 +0530

    create employee Controller

---
+++ src/main/java/lk/ijse/ems/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package lk.ijse.ems.controller;
+
+import lk.ijse.ems.dto.EmployeeDTO;
+import lk.ijse.ems.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    //Build add Employee REST API
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO){
+        EmployeeDTO savedEmployee = employeeService.createEmployee(employeeDTO);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
